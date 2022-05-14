5ff789d29a3c95183fa4e12c37dab52e85c58cf6
RajeshPachaikani <rajeshpachaikani@live.in>
2022-05-14 17:57:37 +0530

    Added pin_assignments.rs

--- src/main.rs
+++ src/main.rs
@@ -1,149 +1,257 @@
 #![deny(unsafe_code)]
-// #![deny(warnings)]
 #![no_main]
 #![no_std]
 
-use panic_semihosting as _;
+/*
+    Author: Rajesh Pachaikani
+    Date: 09/05/2022
+    Title: Firmware for filament extrusion line control and monitoring
+    Description:
+        Pin Configuration:
+            - PA3: Winder Motor Pulse ( Connected to Timer 3 Channel 1)
+            - PA4: Winder Motor Direction
+            - PB1: Linear Guide Motor Pulse ( Connected to Timer 2 Channel 2)
+            - PB10: Linear Guide Motor Direction
+            - PB14: Push Button to reverse the direction of Winder Motor
+            - PB13: Push Button for Spool Change
+            - PB15: Push Button for Fast Forward Winder Motor
+            - PB12: Push Button for Start/Stop both Motors
+            - PA1: Proximity Sensor
+            - PB6: I2C SCL
+            - PB7: I2C SDA
+            - PA11: USB -ve Line
+            - PA12: USB +ve Line
+            - PA6: Winder motor speed connected to ADC1_IN1
+            - PA7: Linear guide motor speed connected to ADC1_IN2
+            - PA2: Linear guide limit switch
+*/
+use rtic::app;
 
-#[rtic::app(device = hal::pac, peripherals = true)]
+mod pin_assignments;
+
+use core::sync::atomic::AtomicBool;
+
+static RUN_STATUS: AtomicBool = AtomicBool::new(false);
+
+#[app(device = hal::stm32, peripherals = true)]
 mod app {
-    use cortex_m::asm::delay;
-    use cortex_m_semihosting::hprintln;
-    use hal::{
+    // Include Start
+    use core::sync::atomic::Ordering;
+    use core::fmt::Write;
+    use heapless::String;
+    use panic_semihosting as _;
+    use hal::
+    {
         prelude::*,
-        stm32,
-        stm32::Interrupt,
+        gpio,
+        i2c::{
+            BlockingI2c,
+            DutyCycle,
+            Mode,
+        },
+        pac::I2C1,
         gpio::{
-            Pin,
             Alternate,
-            Edge,
-            Output,
-            Input,
-            PushPull,
-            PullUp,
             OpenDrain,
-            ExtiPin,
-            gpioa::*,
-            gpiob::*,
-            gpioc::*,
+            PB6,
+            PB7,
         },
     };
-    use hal::pac::rtc::CRH;
-    use rtic::export::monotonic;
+    use hal::gpio::{Edge, ExtiPin, Output, PC13, PushPull};
+    use rtic::mutex_prelude::TupleExt02;
+    use crate::{pin_assignments, RUN_STATUS};
+    use crate::pin_assignments::PinAssignments;
     use systick_monotonic::*;
+    // Include End
 
-    // A monotonic timer to enable scheduling in RTIC
     #[monotonic(binds = SysTick, default = true)]
     type MyMono = Systick<1000>;
 
-    pub enum PressedButton {
-        Fast,
-        Stop,
-        Start,
-        Reverse,
+    pub struct PushButtons {
+        fast_forward: gpio::gpiob::PB15<gpio::Input<gpio::PullUp>>,
+        start_stop: gpio::gpiob::PB12<gpio::Input<gpio::PullUp>>,
+        spool_change: gpio::gpiob::PB13<gpio::Input<gpio::PullUp>>,
+        winder_motor_reverse: gpio::gpiob::PB14<gpio::Input<gpio::PullUp>>,
     }
 
-    pub struct Buttons {
-        fast: PB0<Input<PullUp>>,
-        stop: PA9<Input<PullUp>>,
-        start: PB10<Input<PullUp>>,
-        reverse: PB1<Input<PullUp>>,
+    pub struct ButtonStates {
+        fast_forward: bool,
+        start_stop: bool,
+        spool_change: bool,
+        winder_motor_reverse: bool,
     }
 
     #[shared]
     struct Shared {
-        buttons: Buttons,
-        led: PC13<Output<PushPull>>,
-        button_state: bool,
+        push_buttons: PushButtons,
+        button_states: ButtonStates,
     }
 
     #[local]
     struct Local {
-        time: fugit::Instant<u64, 1_u32, 1000_u32>,
+        i2c: BlockingI2c<I2C1, (PB6<Alternate<OpenDrain>>, PB7<Alternate<OpenDrain>>)>,
+        delay: hal::timer::delay::Delay<hal::stm32::TIM4, 10000>,
+        led: PC13<Output<PushPull>>,
+        debounce_time_variable: u64,
     }
 
     #[init]
-    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
-        let cp: cortex_m::Peripherals = cx.core;
-        let dp: stm32::Peripherals = cx.device;
-
-        let mut rcc = dp.RCC.constrain();
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics)
+    {
+        let mut dp = cx.device;
+        let mut cp: cortex_m::Peripherals = cx.core;
         let mut flash = dp.FLASH.constrain();
+        let rcc = dp.RCC.constrain();
         let mut afio = dp.AFIO.constrain();
 
-        let clocks = rcc.cfgr
+        let systick = cp.SYST;
+
+        cp.SCB.set_sleepdeep();
+
+        let _clocks = rcc.cfgr
             .use_hse(8.MHz())
             .sysclk(72.MHz())
             .freeze(&mut flash.acr);
 
-        let systick = cp.SYST;
-        let mono = Systick::new(systick, clocks.sysclk().to_Hz());
+        let mono = Systick::new(systick, 48_000_000);
 
-        let mut gpioa = dp.GPIOA.split();
-        let mut gpiob = dp.GPIOB.split();
-        let mut gpioc = dp.GPIOC.split();
+        let gpioa = dp.GPIOA.split();
+        let gpiob = dp.GPIOB.split();
+        let gpioc = dp.GPIOC.split();
 
-        // Configure interrupts in PIN A9, B0, B10, B1
-        let mut fast = gpiob.pb0.into_pull_up_input(&mut gpiob.crl);
-        fast.make_interrupt_source(&mut afio);
-        fast.trigger_on_edge(&dp.EXTI, Edge::Falling);
-        fast.enable_interrupt(&dp.EXTI);
+        let all_pins: PinAssignments = pin_assignments::AssignPins::new(
+            gpioa,
+            gpiob,
+            gpioc,
+        );
 
-        let mut stop = gpioa.pa9.into_pull_up_input(&mut gpioa.crh);
-        stop.make_interrupt_source(&mut afio);
-        stop.trigger_on_edge(&dp.EXTI, Edge::Rising);
-        stop.enable_interrupt(&dp.EXTI);
 
-        let mut start = gpiob.pb10.into_pull_up_input(&mut gpiob.crh);
-        start.make_interrupt_source(&mut afio);
-        start.trigger_on_edge(&dp.EXTI, Edge::Rising);
-        start.enable_interrupt(&dp.EXTI);
+        let i2c = BlockingI2c::i2c1(
+            dp.I2C1,
+            (all_pins.i2c_scl, all_pins.i2c_sda),
+            &mut afio.mapr,
+            Mode::Fast {
+                frequency: 5000.Hz(),
+                duty_cycle: DutyCycle::Ratio16to9,
+            },
+            _clocks,
+            1000,
+            10,
+            1000,
+            1000,
+        );
+        let delay = dp.TIM4.delay::<10000>(&_clocks);
 
-        let mut reverse = gpiob.pb1.into_pull_up_input(&mut gpiob.crl);
-        reverse.make_interrupt_source(&mut afio);
-        reverse.trigger_on_edge(&dp.EXTI, Edge::Rising);
-        reverse.enable_interrupt(&dp.EXTI);
-
-        let mut btns = Buttons {
-            fast,
-            stop,
-            start,
-            reverse,
+        let mut push_buttons = PushButtons {
+            fast_forward: all_pins.fast_forward,
+            start_stop: all_pins.start_stop,
+            spool_change: all_pins.spool_change,
+            winder_motor_reverse: all_pins.winder_motor_reverse,
         };
 
-        let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-        led.set_high();
+        let mut led = all_pins.led;
+        led.set_low();
 
-        let button_state = false;
+        // Proximity Sensor
+        let mut proximity_sensor = all_pins.proximity_sensor;
+        proximity_sensor.trigger_on_edge(&dp.EXTI, Edge::Falling);
 
-        // Set monotonic timer to zero
-        let time = monotonics::now();
+        //Winder Reverse Button
+        push_buttons.winder_motor_reverse.trigger_on_edge(&dp.EXTI, Edge::Falling);
+        push_buttons.winder_motor_reverse.make_interrupt_source(&mut afio);
+        push_buttons.winder_motor_reverse.enable_interrupt(&dp.EXTI);
 
-        (Shared { buttons: btns, led, button_state }, Local { time }, init::Monotonics(mono))
+        //Spool change button
+        push_buttons.spool_change.trigger_on_edge(&dp.EXTI, Edge::Falling);
+        push_buttons.spool_change.make_interrupt_source(&mut afio);
+        push_buttons.spool_change.enable_interrupt(&dp.EXTI);
+
+        //Fast forward button
+        push_buttons.fast_forward.trigger_on_edge(&dp.EXTI, Edge::Falling);
+        push_buttons.fast_forward.make_interrupt_source(&mut afio);
+        push_buttons.fast_forward.enable_interrupt(&dp.EXTI);
+
+        //Start/Stop button
+        push_buttons.start_stop.trigger_on_edge(&dp.EXTI, Edge::Falling);
+        push_buttons.start_stop.make_interrupt_source(&mut afio);
+        push_buttons.start_stop.enable_interrupt(&dp.EXTI);
+
+        let button_states = ButtonStates {
+            fast_forward: false,
+            start_stop: false,
+            spool_change: false,
+            winder_motor_reverse: false,
+        };
+        let debounce_time_variable: u64 = 0;
+        (Shared { push_buttons, button_states }, Local { i2c, delay, led, debounce_time_variable }, init::Monotonics(mono))
     }
 
-    #[idle]
-    fn idle(_cx: idle::Context) -> ! {
-        loop {
-            cortex_m::asm::wfi();
+    fn get_button_states(push_buttons: &mut PushButtons) -> ButtonStates {
+        ButtonStates {
+            fast_forward: push_buttons.fast_forward.is_low(),
+            start_stop: push_buttons.start_stop.is_low(),
+            spool_change: push_buttons.spool_change.is_low(),
+            winder_motor_reverse: push_buttons.winder_motor_reverse.is_low(),
         }
     }
 
+    // Task to check the start/stop button PB12
+    #[task(binds = EXTI15_10, shared = [push_buttons, button_states], local = [led, debounce_time_variable])]
+    fn exti15_10(cx: exti15_10::Context) {
+        let led = cx.local.led;
+        let debounce_time_variable = cx.local.debounce_time_variable;
+        (cx.shared.push_buttons, cx.shared.button_states)
+            .lock(|push_buttons, button_states| {
+                let button_states_n = get_button_states(push_buttons);
+                if (button_states_n.start_stop != button_states.start_stop)
+                    || (button_states_n.fast_forward != button_states.fast_forward)
+                    || (button_states_n.spool_change != button_states.spool_change)
+                    || (button_states_n.winder_motor_reverse != button_states.winder_motor_reverse){
+                    *debounce_time_variable = monotonics::now().ticks();
+                }
+                if monotonics::now().ticks() - *debounce_time_variable > 1000 {
+                    if button_states_n.fast_forward {
+                        let status = RUN_STATUS.load(Ordering::Relaxed);
+                        RUN_STATUS.store(!status, Ordering::Relaxed);
+                        led.toggle();
+                    }
+                    if button_states_n.start_stop {
+                        let status = RUN_STATUS.load(Ordering::Relaxed);
+                        RUN_STATUS.store(!status, Ordering::Relaxed);
+                        led.toggle();
+                    }
 
-    #[task(binds = EXTI0, shared = [buttons, led, button_state], local = [time])]
-    fn exti0(cx: exti0::Context) {
-        // let past = cx.local.time;
-        let now = monotonics::now();
-        let mut led = cx.shared.led;
-        if now.ticks() - cx.local.time.ticks() > 500 {
-            *cx.local.time = monotonics::now();
-            led.lock(|led| {
-                led.toggle();
+                }
+                push_buttons.start_stop.clear_interrupt_pending_bit();
+                push_buttons.fast_forward.clear_interrupt_pending_bit();
+                push_buttons.spool_change.clear_interrupt_pending_bit();
+                push_buttons.winder_motor_reverse.clear_interrupt_pending_bit();
+                *button_states = button_states_n;
             });
+    }
+
+    #[idle(local = [i2c, delay])]
+    fn idle(cx: idle::Context) -> ! {
+        let i2c = cx.local.i2c;
+        let delay = cx.local.delay;
+
+        let mut lcd = lcd_lcm1602_i2c::Lcd::new(i2c, delay)
+            .address(0x27)
+            .rows(2)
+            .init()
+            .unwrap();
+
+        lcd.clear().unwrap();
+        loop {
+            let running = RUN_STATUS.load(Ordering::Relaxed);
+            if running {
+                lcd.write_str("Running").unwrap();
+                lcd.set_cursor(0, 0).unwrap();
+            } else {
+                lcd.write_str("Stopped").unwrap();
+                lcd.set_cursor(0, 0).unwrap();
+            }
         }
-        let mut buttons = cx.shared.buttons;
-        buttons.lock(|buttons| {
-            buttons.fast.clear_interrupt_pending_bit();
-        });
     }
 }
+
---
+++ src/pin_assignments.rs
@@ -0,0 +1,81 @@
+/*
+    Pin Configuration:
+            - PA3: Winder Motor Pulse ( Connected to Timer 3 Channel 1)
+            - PA4: Winder Motor Direction
+            - PB1: Linear Guide Motor Pulse ( Connected to Timer 2 Channel 2)
+            - PB10: Linear Guide Motor Direction
+            - PB14: Push Button to reverse the direction of Winder Motor
+            - PB13: Push Button for Spool Change
+            - PB15: Push Button for Fast Forward Winder Motor
+            - PB12: Push Button for Start/Stop both Motors
+            - PA1: Proximity Sensor
+            - PB6: I2C SCL
+            - PB7: I2C SDA
+            - PA11: USB -ve Line
+            - PA12: USB +ve Line
+            - PA6: Winder motor speed connected to ADC1_IN1
+            - PA7: Linear guide motor speed connected to ADC1_IN2
+            - PA2: Linear guide limit switch
+ */
+use hal::{
+    prelude::*,
+    gpio,
+};
+
+
+pub struct PinAssignments {
+    pub winder_motor_pulse: gpio::gpioa::PA3<gpio::Output<gpio::PushPull>>,
+    pub winder_motor_direction: gpio::gpioa::PA4<gpio::Output<gpio::PushPull>>,
+    pub linear_guide_motor_pulse: gpio::gpiob::PB1<gpio::Output<gpio::PushPull>>,
+    pub linear_guide_motor_direction: gpio::gpiob::PB10<gpio::Output<gpio::PushPull>>,
+    pub winder_motor_reverse: gpio::gpiob::PB14<gpio::Input<gpio::PullUp>>,
+    pub spool_change: gpio::gpiob::PB13<gpio::Input<gpio::PullUp>>,
+    pub fast_forward: gpio::gpiob::PB15<gpio::Input<gpio::PullUp>>,
+    pub start_stop: gpio::gpiob::PB12<gpio::Input<gpio::PullUp>>,
+    pub proximity_sensor: gpio::gpioa::PA1<gpio::Input<gpio::PullUp>>,
+    pub i2c_scl: gpio::gpiob::PB6<gpio::Alternate<gpio::OpenDrain>>,
+    pub i2c_sda: gpio::gpiob::PB7<gpio::Alternate<gpio::OpenDrain>>,
+    // pub usb_neg: gpio::gpioa::PA11<gpio::Input<gpio::PullUp>>,
+    // pub usb_pos: gpio::gpioa::PA12<gpio::Input<gpio::PullUp>>,
+    pub winder_motor_speed: gpio::gpioa::PA6<gpio::Analog>,
+    pub linear_guide_motor_speed: gpio::gpioa::PA7<gpio::Analog>,
+    pub linear_guide_limit_switch: gpio::gpioa::PA2<gpio::Input<gpio::PullUp>>,
+    pub led: gpio::gpioc::PC13<gpio::Output<gpio::PushPull>>,
+}
+
+pub trait AssignPins {
+    fn new(
+        gpioa: hal::gpio::gpioa::Parts,
+        gpiob: hal::gpio::gpiob::Parts,
+        gpioc: hal::gpio::gpioc::Parts)
+        -> Self;
+}
+
+impl AssignPins for PinAssignments {
+    fn new(
+        gpioa: hal::gpio::gpioa::Parts,
+        gpiob: hal::gpio::gpiob::Parts,
+        gpioc: hal::gpio::gpioc::Parts)
+        -> Self {
+        let mut gpioa = gpioa;
+        let mut gpiob = gpiob;
+        let mut gpioc = gpioc;
+        Self {
+            winder_motor_pulse: gpioa.pa3.into_push_pull_output(&mut gpioa.crl),
+            winder_motor_direction: gpioa.pa4.into_push_pull_output(&mut gpioa.crl),
+            linear_guide_motor_pulse: gpiob.pb1.into_push_pull_output(&mut gpiob.crl),
+            linear_guide_motor_direction: gpiob.pb10.into_push_pull_output(&mut gpiob.crh),
+            winder_motor_reverse: gpiob.pb14.into_pull_up_input(&mut gpiob.crh),
+            spool_change: gpiob.pb13.into_pull_up_input(&mut gpiob.crh),
+            fast_forward: gpiob.pb15.into_pull_up_input(&mut gpiob.crh),
+            start_stop: gpiob.pb12.into_pull_up_input(&mut gpiob.crh),
+            proximity_sensor: gpioa.pa1.into_pull_up_input(&mut gpioa.crl),
+            i2c_scl: gpiob.pb6.into_alternate_open_drain(&mut gpiob.crl),
+            i2c_sda: gpiob.pb7.into_alternate_open_drain(&mut gpiob.crl),
+            led: gpioc.pc13.into_push_pull_output(&mut gpioc.crh),
+            linear_guide_limit_switch: gpioa.pa2.into_pull_up_input(&mut gpioa.crl),
+            linear_guide_motor_speed: gpioa.pa7.into_analog(&mut gpioa.crl),
+            winder_motor_speed: gpioa.pa6.into_analog(&mut gpioa.crl),
+        }
+    }
+}
